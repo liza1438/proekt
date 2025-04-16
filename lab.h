9f2aa1d849a3d6b36edc90fb14ce687ea37cc769
liza1438 <liza_andronova06@mail.ru>
2025-04-16 20:59:02 +0300

    Create lab.h

---
+++ lab.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void lab1();
+void lab1add();
+void lab2();
+void lab2add();
+void lab3();
+void lab3add();
+void lab4();
+void lab4add();
+void lab5();
+void lab5add();
+void lab6();
+void lab6add();
